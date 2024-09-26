98f1a9487eff940f6793126d2d049d8b74d07688
marco22aurelio <133982309+marco22aurelio@users.noreply.github.com>
2024-09-26 16:37:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Basquete',
+    'Qual é o objetivo principal do basquete???',
+    'O objetivo do basquete é marcar mais pontos que o time adversário, lançando a bola na cesta do oponente..'
+)
+
+criaCartao(
+    'Basquete',
+    'Quantos jogadores de cada equipe estão em quadra durante um jogo de basquete??',
+    'Cada equipe tem cinco jogadores em quadra durante um jogo.'
+)
+
+criaCartao(
+    'Basquete',
+    'Qual é a altura da cesta de basquete em competições oficiais??',
+    'A altura da cesta em competições oficiais é de 3,05 metros (10 pés)..  ' 
+
+)
+
+criaCartao(
+    'Basquete',
+    'Qual é a duração de uma partida de basquete na NBA?  ?',
+    'ma partida da NBA é composta por quatro quartos de 12 minutos cada, totalizando 48 minutos de jogo..'
+)
